9b93298f4b458e6f81477a9171f74f2a272c5102
jordyhao <xionghao926@gmail.com>
2014-02-27 14:28:27 +0100

    haoPush
    QQQQQ

---
+++ Dev/TravelJournal/TravelJournal.PCL/BusinessLogic/ExifExtractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TravelJournal.PCL.DataService;
+
+namespace TravelJournal.PCL.BusinessLogic
+{
+    class ExifExtractor:IExifExtractor
+    {
+
+        public DataService.GpsPoint ExtractGeoCoordinate(Photo p)
+        {
+            return p.Point;
+        }
+    }
+}
---
+++ Dev/TravelJournal/TravelJournal.PCL/DataService/DataManagerBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TravelJournal.PCL.DataService
+{
+    abstract class DataManagerBase
+    {
+        private Album album;
+        private User user;
+
+        public DataManagerBase()
+        {
+            album = new Album();
+            user = new User();
+        }
+        public DataManagerBase(Album album, User user)
+        {
+            this.album = album;
+            this.user = user;
+        }
+        public DataManagerBase Load()
+        {
+            return this;
+        }
+
+        public void Save()
+        {
+
+        }
+        public Album AddPhoto(Photo aPhoto)
+        {
+            album.PhotoList.Add(aPhoto);
+            return album;
+        }
+        public Album AddPhoto(IEnumerable<Photo> aPhotoList)
+        {
+            album.PhotoList.AddRange(aPhotoList);
+            return album;
+        }
+        public Album GetAlbum()
+        {
+            return this.album;
+        }
+        public User GetUserInfo()
+        {
+            return this.user;
+        }
+    }
+}
