04e14c39ab61411d81623a57efa8e6005d5db698
jordyhao <xionghao926@gmail.com>
2014-03-08 23:04:24 +0100

    Update WP8 impl. 1

---
+++ Dev/TravelJournal/TravelJournal.WP8/.DataService/WpDataManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Phone.Maps.Services;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelJournal.PCL.DataService;
+using Windows.Devices.Geolocation;
+
+
+namespace TravelJournal.WP8.DataService
+{
+    class WpDataManager
+    {
+
+    }
+}
